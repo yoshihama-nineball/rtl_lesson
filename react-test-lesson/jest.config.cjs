a7fcaf0db86063b0e8cada16acdf003ec1b451fb
ezu0 <burimofumofu@gmail.com>
2025-01-13 17:18:07 +0900

    first-commit

---
+++ react-test-lesson/jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transformIgnorePatterns: ['node_modules/(?!(your-module)/)'],
+};
---
+++ react-test-lesson/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ssr from 'vite-plugin-ssr/plugin';
+
+export default defineConfig({
+  plugins: [react(), ssr()],
+  optimizeDeps: {
+    include: ['@babel/preset-react'],
+  },
+});
