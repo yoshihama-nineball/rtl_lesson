ff6b2bb83ed16236bdca00d93b995aec8be0d80f
ezu0 <burimofumofu@gmail.com>
2025-01-15 08:25:50 +0900

    fix: 設定

--- next-test-lesson/jest.config.ts
+++ next-test-lesson/jest.config.ts
@@ -1,12 +1,21 @@
-module.exports = {
-  preset: 'ts-jest',
+import { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  coverageProvider: 'v8',
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '@/(.*)$': '<rootDir>/src/$1',
+  },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
-  transformIgnorePatterns: ['node_modules/(?!(your-module)/)'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
 };
+
+export default createJestConfig(config);
