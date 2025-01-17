1f586e28a9e2f9cc7ed669f19c5ad0cdb785b237
ezu0 <burimofumofu@gmail.com>
2025-01-17 18:21:47 +0900

    add: output感ず宇賀トリガーされるときのテストケース

--- next-test-lesson/src/tests/_components/RenderInput.test.tsx
+++ next-test-lesson/src/tests/_components/RenderInput.test.tsx
@@ -1,41 +1,53 @@
-import React from 'react';
-import { cleanup, render, screen } from '@testing-library/react';
-import RenderInput from '@/app/_components/RenderInput';
-import userEvent from '@testing-library/user-event';
+import React from 'react'
+import { cleanup, render, screen } from '@testing-library/react'
+import RenderInput from '@/app/_components/RenderInput'
+import userEvent from '@testing-library/user-event'
 
 //MEMO: 次のテストが前のテストの副作用を受けないようにクリーンアップする
-afterEach(() => cleanup());
+afterEach(() => cleanup())
 
 describe('Rendering', () => {
   it('レンダリング要素をすべて正しく取得する', () => {
-    const mockOutputConsole = jest.fn();
-    render(<RenderInput outputConsole={mockOutputConsole} />);
-    screen.debug();
-    expect(screen.getByRole('button')).toBeTruthy();
-    expect(screen.getByPlaceholderText('Enter')).toBeTruthy();
-  });
-});
+    const mockOutputConsole = jest.fn()
+    render(<RenderInput outputConsole={mockOutputConsole} />)
+    screen.debug()
+    expect(screen.getByRole('button')).toBeTruthy()
+    expect(screen.getByPlaceholderText('Enter')).toBeTruthy()
+  })
+})
 
 describe('inputフォームのonChangeイベント', () => {
   it('inputの入力値を正しく取得する', async () => {
-    const mockOutputConsole = jest.fn();
-    render(<RenderInput outputConsole={mockOutputConsole} />);
-    const inputValue = screen.getByPlaceholderText('Enter') as HTMLInputElement;
+    const mockOutputConsole = jest.fn()
+    render(<RenderInput outputConsole={mockOutputConsole} />)
+    const inputValue = screen.getByPlaceholderText('Enter') as HTMLInputElement
     //MEMO: userEvent.typeで入力フィールドにtestを入力
-    await userEvent.type(inputValue, 'test');
-    expect(inputValue.value).toBe('test');
-  });
-});
+    await userEvent.type(inputValue, 'test')
+    expect(inputValue.value).toBe('test')
+  })
+})
 
 describe('Consoleボタンが状況に応してトリガーされる', () => {
   it('output関数はトリガーされない', () => {
-    const mockOutputConsole = jest.fn();
-    render(<RenderInput outputConsole={mockOutputConsole} />);
-    const consoleButton = screen.getByRole('button');
+    const mockOutputConsole = jest.fn()
+    render(<RenderInput outputConsole={mockOutputConsole} />)
+    const consoleButton = screen.getByRole('button')
     //MEMO: userEvent.clickでconsoleボタンをクリック
-    userEvent.click(consoleButton);
+    userEvent.click(consoleButton)
     //MEMO: 空のままボタンを押しているので、実装元のRenderInput.tsxのoutputValueを満たしていないため、mockOutputConsoleは呼ばれない
-    expect(mockOutputConsole).not.toHaveBeenCalled();
+    expect(mockOutputConsole).not.toHaveBeenCalled()
+    //差分テスト
   })
 
+  it('output関数がトリガーされる場合のテストケース', async () => {
+    const mockOutputConsole = jest.fn()
+    render(<RenderInput outputConsole={mockOutputConsole} />)
+    const inputValue = screen.getByPlaceholderText('Enter')
+    await userEvent.type(inputValue, 'test')
+
+    const consoleButton = screen.getByRole('button')
+    await userEvent.click(consoleButton)
+
+    expect(mockOutputConsole).toHaveBeenCalledTimes(1)
+  })
 })
